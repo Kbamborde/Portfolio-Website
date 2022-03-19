d0c4d68e9bad00955f740c4d2361b6ce1c7ecd3b
Kbamborde <kunalbamborde00@gmail.com>
2022-03-19 23:29:59 +0530

    Portfolio

--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+
+import { AppWrap, MotionWrap } from "../../Wrapper";
+import { client, urlFor } from "../../client";
+
 import "./Testimonials.scss";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = `*[_type == "testimonials"]`;
+    const brandsQuery = `*[_type == "brands"]`;
+
+    client.fetch(query).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
+  return <></>;
 };
 
-export default Testimonials;
+export default AppWrap(
+  MotionWrap(Testimonials, "testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
